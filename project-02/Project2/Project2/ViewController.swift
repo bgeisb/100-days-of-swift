d57c8c33ffd941fce3df49d13b9961587aba4122
bgeisb <benedikt@geisberger.biz>
2022-04-17 11:45:00 +0200

    Day 21: Project 2, Challenges

--- project-02/Project2/Project2/ViewController.swift
+++ project-02/Project2/Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var questionsAsked = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +41,14 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = countries[correctAnswer].uppercased()
+        self.navigationItem.setTitle(title: "\(countries[correctAnswer].uppercased()) (\(questionsAsked)/10)", subtitle: "Score: \(score)")
+    }
+    
+    func restartGame(action: UIAlertAction! = nil) {
+        score = 0
+        questionsAsked = 0
+
+        askQuestion()
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
@@ -53,14 +61,47 @@ class ViewController: UIViewController {
             title = "Wrong"
             score -= 1
         }
+        questionsAsked += 1
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        let ac: UIAlertController
+        if questionsAsked < 10 {
+            ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        } else {
+            ac = UIAlertController(title: score >= 5 ? "Good job!" : "You can do better!", message: "Your final score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Restart", style: .default, handler: restartGame))
+        }
         
         present(ac, animated: true)
     }
     
-    
 }
 
+extension UINavigationItem {
+    func setTitle(title:String, subtitle:String) {
+        
+        let one = UILabel()
+        one.text = title
+        one.font = UIFont.systemFont(ofSize: 17)
+        one.sizeToFit()
+        
+        let two = UILabel()
+        two.text = subtitle
+        two.font = UIFont.systemFont(ofSize: 12)
+        two.textAlignment = .center
+        two.sizeToFit()
+        
+        let stackView = UIStackView(arrangedSubviews: [one, two])
+        stackView.distribution = .equalCentering
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        
+        let width = max(one.frame.size.width, two.frame.size.width)
+        stackView.frame = CGRect(x: 0, y: 0, width: width, height: 35)
+        
+        one.sizeToFit()
+        two.sizeToFit()
+        
+        self.titleView = stackView
+    }
+}
